f902f5e0365f734bd1481367edbca72730d35460
Saketh Nimmagadda <45970393+saketh-n@users.noreply.github.com>
2024-01-14 21:27:40 -0800

    Added Responsiveness to Home Page + NavButtons. Note: Last commit before style reviews + tests w sufficient code coverage

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -17,10 +17,64 @@ export const NavButton = (props: {
     transition: "filter 0.3s ease",
   };
 
+  let tailwindStyling = `bg-cover bg-center h-[240px] w-[240px] text-lg rounded-3xl`;
+  // Adding Responsive Design
+
+  // Helper method for responsive design
+  const responsiveStyling = (prefix: string): string => {
+    // Map prefix to scaling coefficient
+    const prefixToScale: Record<string, number> = {
+      sm: 5,
+      md: 6,
+      lg: 7,
+      xl: 8,
+      "2xl": 9,
+    };
+
+    // Map prefix to text-scale
+    const prefixToTextScale: Record<string, string> = {
+      sm: "xl",
+      md: "2xl",
+      lg: "3xl",
+      xl: "4xl",
+      "2xl": "5xl",
+    };
+
+    const buttonSideLength = 60 * prefixToScale[prefix];
+
+    let styling = ``;
+
+    if (prefix in prefixToScale) {
+      styling += ` ${prefix}:h-[${buttonSideLength}px]`;
+      styling += ` ${prefix}:w-[${buttonSideLength}px]`;
+    }
+
+    if (prefix in prefixToTextScale) {
+      styling += ` ${prefix}:text-${prefixToTextScale[prefix]}`;
+    }
+
+    return styling;
+  };
+
+  // Behavior on Small Screens (width > 640px)
+  tailwindStyling += responsiveStyling("sm");
+
+  // Behavior on Medium Screens (width > 768px)
+  tailwindStyling += responsiveStyling("md");
+
+  // Behavior on Large Screens (width > 1024 px)
+  tailwindStyling += responsiveStyling("lg");
+
+  // Behavior on XL Screens (width > 1280 px)
+  tailwindStyling += responsiveStyling("xl");
+
+  // Behavior on 2XL Screens (width > 1536 px)
+  tailwindStyling += responsiveStyling("2xl");
+
   return (
     <button
       style={style}
-      className={`bg-cover bg-center h-[360px] w-[360px] text-2xl rounded-3xl`}
+      className={tailwindStyling}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,11 @@ import measureImprovement from "../images/app/improvement.png";
 
 export const Home = (): React.JSX.Element => {
   return (
-    <div className="h-screen grid grid-cols-2 items-center">
-      <div className="flex justify-center">
+    <div className="h-screen grid md:grid-cols-2 items-center">
+      <div className="flex justify-center items-center md:items-start h-1/2">
         <NavButton img={measureImage} text="Measure Growth" />
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center md:items-start h-1/2">
         <NavButton img={measureImprovement} text="Measure Improvement" />
       </div>
     </div>
