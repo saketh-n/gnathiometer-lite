0a82047a1aa548c24fa46e02d7cedbbd98528f0a
Saketh Nimmagadda <45970393+saketh-n@users.noreply.github.com>
2024-01-15 01:05:38 -0800

    Code Style: Consistency Issue with JSDoc in NavButton

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import { generateStyle, generateTailwindStyling } from "../helpers/nav-styling";
 
-/** NavButtons are used for navigating to one of the functions of Gnathiometer
+/**
+ * @param img The path to the image file we want as the button's background
+ * @param txt The text that the button displays
+ * @returns NavButtons are used for navigating to one of the functions of Gnathiometer
  *    (Measure Forward Growth or Measure Improvement)
  */
 export const NavButton = (props: {
   img: string;
   text: string;
 }): React.JSX.Element => {
-  /**
-   * @param img The path to the image file we want as the button's background
-   * @param txt The text that the button displays
-   */
   const { img, text } = props;
   const [hover, setHover] = useState(false);
 
