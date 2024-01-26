0434ef2c12bc1b70bef86b5e8f6a59bee0b35ec1
Saketh Nimmagadda <45970393+saketh-n@users.noreply.github.com>
2024-01-25 23:50:22 -0800

    Added Growth Guide component

---
+++ src/components/measure/GrowthGuide.tsx
@@ -0,0 +1,16 @@
+import guide from "../../images/app/growth-guide.png";
+
+/**
+ *
+ * Renders the Gnathiometer's Growth Guide and ensures it appears on top
+ * of other draggable elements in the application.
+ * @returns {React.JSX.Element} Gnathiometer Growth Guide.
+ */
+export const GrowthGuide = (): React.JSX.Element => {
+  // Make sure the Growth Guide always overlays any image
+  const guideStyle: React.CSSProperties = {
+    position: "relative",
+    zIndex: 1000,
+  };
+  return <img src={guide} alt="Growth Guide" style={guideStyle} />;
+};
