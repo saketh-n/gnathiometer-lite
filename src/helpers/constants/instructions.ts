da51eb74d960218502bef4b068380f0c0cb3c300
Saketh Nimmagadda <45970393+saketh-n@users.noreply.github.com>
2024-03-02 16:08:16 -0800

    Condensed Measure Growth Instructions

--- src/helpers/constants/instructions.ts
+++ src/helpers/constants/instructions.ts
@@ -1,10 +1,6 @@
 export const measureGrowthInstructions = [
-  "Drag the end of the 5CM Marker to Point N on the Growth Guide",
-  "Rotate the image so that the marker is parallel and on the line from point F through N",
-  "Scale up the image so that the 5CM Marker starts at Point F and ends at point N",
-  "Return the Patient Image to normal rotation, but maintain the scale",
-  "Superimpose the Patient Image on Point N",
-  "Rotate the Patient Image so that Point F touches the soft tissue of the forehead above",
+  "Scale the Image Properly (By Matching the 5 CM Marker on the Image w/ the Growth Guide)",
+  "Align Point F & N on the Growth Guide with the Patient Image",
   "Place the Chin Marker on the Gnathion (forwardmost and downwardmost point on the Chin)",
   "Ensure everything looks good, otherwise go back to fix it, click Next to compute Angle & Gnathion Position",
   "Press Next to Print Results",
