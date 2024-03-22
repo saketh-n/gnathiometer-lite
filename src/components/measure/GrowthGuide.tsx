5261d8ac479e83d03d68eebf632a81abec5b0ceb
Saketh Nimmagadda <45970393+saketh-n@users.noreply.github.com>
2024-03-21 17:48:23 -0700

    After first Nav instruction, don't show 5 CM Marker

--- src/components/measure/GrowthGuide.tsx
+++ src/components/measure/GrowthGuide.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import guide from "../../images/app/growth-guide.png";
+import guideNo5CM from "../../images/app/growth-guide-no-5cm-marker.png";
 import { MeasureContext } from "../../contexts/MeasureContext";
 import { ChinMarker } from "./ChinMarker";
 
@@ -10,11 +11,16 @@ import { ChinMarker } from "./ChinMarker";
  * @returns {React.JSX.Element} Gnathiometer Growth Guide.
  */
 export const GrowthGuide = (): React.JSX.Element => {
-  const { chinMarkerEnabled } = useContext(MeasureContext);
+  const { chinMarkerEnabled, instructionIndex: index } =
+    useContext(MeasureContext);
+
+  // The first instruction is to scale the image, after that we don't want
+  // growth guide to have a 5CM Marker
+  const guideImg = index >= 1 ? guideNo5CM : guide;
 
   return (
     <div className="relative">
-      <img src={guide} alt="Growth Guide" data-testid="growth-guide" />
+      <img src={guideImg} alt="Growth Guide" data-testid="growth-guide" />
       {chinMarkerEnabled && <ChinMarker />}
     </div>
   );
